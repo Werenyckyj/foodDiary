60dfba031ee2db4d828d57721f5ffacf04515a4d
Werenyckyj <werenyckyjvojtech@gmail.com>
2022-12-22 15:40:44 +0100

    Add project files.

---
+++ FoodDiary/FoodDiary/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Raleway-Light.ttf", Alias = "RalewayL")]
+[assembly: ExportFont("Raleway-Medium.ttf", Alias = "RalewayM")]
+[assembly: ExportFont("Raleway-Bold.ttf", Alias = "RalewayB")]
